61b3496d16b034508f521aec3b873fa03d5ef20d
bayu-aji-nugroho <bayuastudies@gmail.com>
2025-01-13 18:06:57 +0700

    mengatur ulang semua layout

---
+++ webKopi/tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+    content: ['./*.{html,js}'], // Sesuaikan dengan struktur proyek
+    theme: {
+      extend: {
+        backgroundImage: {
+          'poto': "url('./gambar-biji-kopi.jpeg')"
+        }
+      },
+    },
+    plugins: [],
+  };
+  
